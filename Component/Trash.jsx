be7f303ed9b045255a80aca191b3cc1ef29d9766
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2024-01-31 22:16:24 +0300

    Create Site İtems

---
+++ Component/Trash.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const TrashIcon = () => {
+  return <FontAwesomeIcon icon={faTrash} />;
+}
+
+export default TrashIcon;
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,143 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import TrashIcon from "@/Component/Trash";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [number, setNumber] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [number3, setNumber3] = useState(0);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="Navbar">
+        <h1>Logo</h1>
+      </div>
+      <div className="Box1">
+        <img
+          className="Photo"
+          src="https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/141033-1_large.jpg"
+          alt="İphone Photo"
+        ></img>
+        <h1>İphone 15</h1>
+        <button
+          className="Select"
+          onClick={() => {
+            setNumber(number + 1);
+          }}
+        >
+          $10
+        </button>
+      </div>
+      <div className="Box2">
+        <img
+          className="Photo"
+          src="https://images.philips.com/is/image/philipsconsumer/fd14e69c14ae43eebe24ae6800fac6c4?$jpglarge$&wid=1250"
+          alt="Coffe mac."
+        ></img>
+        <h1>Philips Coffe Mac.</h1>
+        <button
+          className="Select"
+          onClick={() => {
+            setNumber1(number1 + 1);
+          }}
+        >
+          $15
+        </button>
+      </div>
+      <div className="Box3">
+        <img
+          className="Photo"
+          src="https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/leap-petite-global/dynamic-media/personal-care/308c/primary/400690-01.png?scl=1&fmt=png-alpha"
+          alt="Dyson"
+        ></img>
+        <h1>Dyson Airwrap</h1>
+        <button
+          className="Select"
+          onClick={() => {
+            setNumber2(number2 + 1);
+          }}
+        >
+          $20{" "}
+        </button>
+      </div>
+      <div className="Box4">
+        <img
+          className="Photo"
+          src="https://pic.otoyazar.com/wp-content/uploads/2017/08/2018-BMW-M5.jpg"
+          alt="Car Photo"
+        ></img>
+        <h1>Bmw M5</h1>
+        <button
+          className="Select"
+          onClick={() => {
+            setNumber3(number3 + 1);
+          }}
+        >
+          $100
+        </button>
+      </div>
+      <div className="Card">
+        <h1>Your Cart</h1>
+        <hr />
+        <h2>PRODUCTS & QUANTITY</h2>
+        <p className="Card-item">
+          {" "}
+          {number} x İphone 15{" "}
+          <button
+            className="Mybut"
+            onClick={() => {
+              setNumber(number - 1);
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            -1
+          </button>{" "}
+        </p>
+        <p className="Card-item">
+          {" "}
+          {number1} x Philips Coffe Mac.{" "}
+          <button
+            className="Mybut"
+            onClick={() => {
+              setNumber1(number1 - 1);
+            }}
+          >
+            -1
+          </button>{" "}
+        </p>
+
+        <p className="Card-item">
+          {" "}
+          {number2} x Dyson Airwrap{" "}
+          <button
+            className="Mybut"
+            onClick={() => {
+              setNumber2(number2 - 1);
+            }}
+          >
+            -1
+          </button>{" "}
+        </p>
+        <p className="Card-item">
+          {" "}
+          {number3} x Bmw M5{" "}
+          <button
+            className="Mybut"
+            onClick={() => {
+              setNumber3(number3 - 1);
+            }}
+          >
+            -1
+          </button>{" "}
+        </p>
+        <hr />
+        <h1>Total Price</h1>
+        <p>{number * 10 + number1 * 15 + number2 * 20 + number3 * 100} $</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
